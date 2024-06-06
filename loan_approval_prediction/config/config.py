48c80cffc965ae593b5c20bddaac8d8aca30bbd8
Rahul <rahul.vasala98@gmail.com>
2024-06-06 09:09:37 +0530

    Preprocessing code added

--- loan_approval_prediction/config/config.py
+++ loan_approval_prediction/config/config.py
@@ -6,9 +6,28 @@ PACKAGE_ROOT = pathlib.Path(loan_approval_prediction.__file__).resolve().parent
 
 DATA_PATH = pathlib.Path.joinpath(PACKAGE_ROOT, "datasets")
 
+SAVED_MODEL_NAME = "loan_classification.pkl"
 SAVED_MODELS_PATH = pathlib.Path.joinpath(PACKAGE_ROOT, "trained_models")
 
 TRAIN_DF = "train.csv"
 TEST_DF = "test.csv"
 
-TARGET = ""
+TARGET = "Loan_Status"
+
+FEATURES = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed',
+            'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term',
+            'Credit_History', 'Property_Area']
+
+NUM_FEATURES = ['ApplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
+
+CAT_FEATURES = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Credit_History', 'Property_Area']
+
+
+FEATURES_TO_ENCODE = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Credit_History', 'Property_Area']
+
+FEATURE_TO_MODIFY = 'ApplicantIncome'
+FEATURE_TO_ADD = 'CoapplicantIncome'
+
+DROP_FEATURES = ['CoapplicantIncome']
+
+LOG_FEATURES = ['ApplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
---
+++ loan_approval_prediction/preprocessing.py
@@ -0,0 +1,78 @@
+from loan_approval_prediction.config import config
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class MeanImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, variables = None):
+        self.variables = variables
+        self.mean_dict = {}
+
+    def fit(self, X, y=None):
+        for var in self.variables:
+            self.mean_dict[var] = X[var].mean()
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for var in self.variables:
+            X[var].fillna(self.mean_dict[var], axis=1, inplace=True)
+        return X
+
+
+class ModeImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, variables=None):
+        self.variables = variables
+        self.mean_dict = {}
+
+    def fit(self, X, y=None):
+        for var in self.variables:
+            self.mean_dict[var] = X[var].mode()[0]
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for var in self.variables:
+            X[var].fillna(self.mean_dict[var], axis=1, inplace=True)
+        return X
+
+
+class DropColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, variables=None):
+        self.variables = variables
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        X.drop(self.variables, axis=1, inplace=True)
+        return X
+
+
+class AddingVariables(BaseEstimator, TransformerMixin):
+    def __init__(self, variables=None, ref_variable=None):
+        self.variables = variables
+        self.ref_variable = ref_variable
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        X[self.variables] = X[self.variables] + X[self.ref_variable]
+        return X
+
+
+class LogTransformation(BaseEstimator, TransformerMixin):
+    def __init__(self, variables=None):
+        self.variables = variables
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for var in self.variables:
+            X[var] = np.log(X[var])
+        return X
+
